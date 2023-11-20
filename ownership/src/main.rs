4da46d57af7bf3de13d32be8f90770a8c815e8b9
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-11-20 20:00:35 +0700

    Add the example of the slice type

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -72,6 +72,49 @@ fn main() {
 
     let reference_to_nothing = dangle();
     println!("{}", reference_to_nothing);
+
+    // let mut s = String::from("hello world");
+    // let word = first_word(&s);
+    // println!("{word}");
+    // s.clear();
+
+    let s = String::from("hello world");
+    let hello = &s[0..5];
+    let world = &s[6..11];
+    println!("{} {}", hello, world);
+
+    let s = String::from("hello");
+    let slice = &s[0..2];
+    println!("{}", slice);
+    let slice = &s[..2];
+    println!("{}", slice);
+
+    let len = s.len();
+    let slice = &s[3..len];
+    println!("{}", slice);
+    let slice = &s[3..];
+    println!("{}", slice);
+
+    let mut s = String::from("hello world");
+    let word = first_word(&s);
+
+    // s.clear(); // error!
+
+    println!("the first word is: {}", word);
+
+    let my_string = String::from("hello world");
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+    let word = first_word(my_string_literal);
+
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+    assert_eq!(slice, &[2, 3]);
 }
 
 fn take_ownership(some_string: String) {
@@ -112,4 +155,28 @@ fn change(some_string: &mut String) {
 fn dangle() -> String {
     let s = String::from("hello");
     s
+}
+
+// fn first_word(s: &String) -> usize {
+//     let bytes = s.as_bytes();
+
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return i;
+//         }
+//     }
+
+//     return s.len();
+// }
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
