e5e7a35f8e4e49e05edecd34436ff04eb47b957c
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-11-20 19:23:19 +0700

    Add the example of references and borrowing

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -33,8 +33,45 @@ fn main() {
     let s3 = takes_and_gives_back(s2);
 
     let s1 = String::from("hello");
-    let (s2, len) = calculate_length(s1);
-    println!("The length of '{}' is '{}'.", s2, len);
+    // let (s2, len) = calculate_length(s1);
+    // println!("The length of '{}' is '{}'.", s2, len);
+    let len = calculate_length(&s1);
+    println!("The length of '{}' is {}.", s1, len);
+
+    let mut s = String::from("hello");
+    change(&mut s);
+    println!("{}", s);
+
+    // let r1 = &mut s;
+    // let r2 = &mut s;
+    // println!("{}, {}", r1, r2);
+
+    {
+        let r1 = &mut s;
+        println!("{}", r1);
+    }
+
+    let r2 = &mut s;
+    println!("{}", r2);
+
+    let r1 = &s;
+    let r2 = &s;
+    println!("{}, {}", r1, r2);
+
+    // let r1 = &s;
+    // let r2 = &s;
+    // let r3 = &mut s;
+    // println!("{}, {}, {}", r1, r2, r3);
+
+    let r1 = &s;
+    let r2 = &s;
+    println!("{}, {}", r1, r2);
+
+    let r3 = &mut s;
+    println!("{}", r3);
+
+    let reference_to_nothing = dangle();
+    println!("{}", reference_to_nothing);
 }
 
 fn take_ownership(some_string: String) {
@@ -54,7 +91,25 @@ fn takes_and_gives_back(a_string: String) -> String {
     a_string
 }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len();
-    (s, length)
+// fn calculate_length(s: String) -> (String, usize) {
+//     let length = s.len();
+//     (s, length)
+// }
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+// fn dangle() -> &String {
+//     let s = String::from("hello");
+//     &s
+// }
+
+fn dangle() -> String {
+    let s = String::from("hello");
+    s
 }
